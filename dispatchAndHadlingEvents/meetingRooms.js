3033ff696f13a5306e67bb711f4b2d246639a3d9
vibinmky <57066938+vibinmky@users.noreply.github.com>
2020-07-10 13:58:31 +0530

    Create meetingRooms.js

---
+++ dispatchAndHadlingEvents/meetingRooms.js
@@ -0,0 +1,31 @@
+import {
+  LightningElement
+} from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+
+  meetingRoomsInfo = [{
+      Name: 'A-1',
+      Capacity: '12'
+    },
+    {
+      Name: 'B-1',
+      Capacity: '10'
+    },
+    {
+      Name: 'C-1',
+      Capacity: '7'
+    },
+    {
+      Name: 'C-2',
+      Capacity: '8'
+    }
+  ];
+
+  selectedRoomName;
+
+  tileSelected(event) {
+    const room = event.detail;
+    this.selectedRoomName = room.Name;
+  }
+}
