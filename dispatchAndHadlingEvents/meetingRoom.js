6fbe627d1d5f0e9312871009cab691dae3073286
vibinmky <57066938+vibinmky@users.noreply.github.com>
2020-07-10 13:57:51 +0530

    Create meetingRoom.js

---
+++ dispatchAndHadlingEvents/meetingRoom.js
@@ -0,0 +1,17 @@
+import {
+  LightningElement,
+  api
+} from 'lwc';
+
+export default class MeetingRoom extends LightningElement {
+  @api meetingRoomInfo; //{Name: 'A-1' , Capacity: '12' }
+  @api show = false;
+
+  selectedMeetingRoom() {
+    // create custom event then dispatch it
+    const selectedRoomEvent = new CustomEvent('selectedroomevent', {
+      detail: this.meetingRoomInfo
+    });
+    this.dispatchEvent(selectedRoomEvent);
+  }
+}
