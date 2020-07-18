d2fccf540447c6555eec446678b49cfdf04cc7a8
vibinmky <57066938+vibinmky@users.noreply.github.com>
2020-07-18 12:14:35 +0530

    Update accountRecordFormLDS.js

--- createViewRecords/accountRecordFormLDS.js
+++ createViewRecords/accountRecordFormLDS.js
@@ -1,21 +1,26 @@
 import {
-  LightningElement
+  LightningElement,
+  api
 } from 'lwc';
 // While editing fields name is salesforce to show this LWC dependency
 // use the hardcode declaration like below
+/*
 import Name_Field from '@salesforce/schema/Account.Name';
 import Phone_Field from '@salesforce/schema/Account.Phone';
 import Website_Field from '@salesforce/schema/Account.Website';
+*/
 
 export default class AccountRecordFormLDS extends LightningElement {
 
-  recordId;
+  @api recordId;
+  @api objectApiName;
 
-  fieldsArray = [
-    Name_Field,
-    Phone_Field,
-    Website_Field
-  ];
+  /* fieldsArray = [
+     Name_Field,
+     Phone_Field,
+     Website_Field
+   ];
+   */
   successHandler(event) {
     this.recordId = event.detail.Id;
   }
